cc01d2cd34afe95368ae600591d441563074bfbb
Brian Key <briankey@u.northwestern.edu>
2025-07-18 14:01:28 -0500

    Prepped data viz. for ch3

---
+++ scripts/ch3/5b_viz_results_make.R
@@ -0,0 +1,9 @@
+# load packages ----
+library(tidyverse)
+library(here)
+library(knitr)
+
+# load data ----
+load(here("data/ch3/visualizations/imp_dml_pool_2fe_viz_dfs.rda"))
+load(here("data/ch3/visualizations/imp_dml_pool_spat_regfe_viz_dfs.rda"))
+
